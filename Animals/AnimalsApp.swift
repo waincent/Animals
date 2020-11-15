132fb069803c96493e04e61a87a5fb7a47f7e032
waincent <wangbin@gmail.com>
2020-11-15 22:47:50 +0800

    init project

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Waincent on 2020/11/15.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animals/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animals
+//
+//  Created by Waincent on 2020/11/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
